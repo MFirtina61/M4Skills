0d51f4ff3a5efe4a2ec1e00ce907d708b3a4a2f7
MFirtina61 <114576186+MFirtina61@users.noreply.github.com>
2023-06-20 09:45:03 +0200

    Add files via upload

---
+++ M4SKILS/M4SKILS-main/afhandeling.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $naam = $_POST['naam'];
+    $email = $_POST['email'];
+
+    $_SESSION['naam'] = $naam;
+    $_SESSION['email'] = $email;
+
+    echo "Naam:" . $naam . "<br>";
+    echo "Email:" . $email . "<br>";
+
+    header("Location: afhandeling.php");
+?>
